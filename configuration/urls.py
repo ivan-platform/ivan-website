f00a77c172b87de61024b5f5a0e7b66b47f63025
ivan-platform <98419047+ivan-platform@users.noreply.github.com>
2022-02-10 02:38:05 -0500

    updates for ivan-dashboard

--- configuration/urls.py
+++ configuration/urls.py
@@ -22,15 +22,23 @@ from django.contrib.auth import views as auth_views
 from django.urls.conf import include
 
 from mainapp import views
+from configuration import views as frontend_views
 
 app_name = 'mainapp'
+
+vue_urls = [
+    path('', frontend_views.frontend),
+    # path('vue/', frontend_views.frontend),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('', include('mainapp.urls')),
+    path('vuehome/', include(vue_urls)),
+    url('', include('mainapp.urls'), name='home'),
     url('^accounts/register/$', views.RegisterView.as_view(), name='register'),
     url('^accounts/login/$', views.LoginView.as_view(), name='login'),
     url('^accounts/logout/$', auth_views.LogoutView.as_view(),name='logout'),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
